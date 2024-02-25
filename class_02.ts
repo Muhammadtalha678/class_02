e132d65bc7568194dba6c6030c7cd31fbf67882b
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-02-25 14:48:13 +0500

    class

---
+++ class_02.ts
@@ -0,0 +1,3 @@
+// -D (apni application pr jo ha load ni daln ga ma apny liye ya dependencies krrha hn)
+
+// gitignore jo hy wo nodemodules ki files ko ni push kry ga git ignore kun ky ya jo ha dev ky liye ha
