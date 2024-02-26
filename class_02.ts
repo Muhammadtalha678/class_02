d135edaca8ce131560926c0a86d335ca4b1ce41d
Muhammadtalha678 <muhammadtalha0110@gmail.com>
2024-02-27 01:30:16 +0500

    Q-117

--- class_02.ts
+++ class_02.ts
@@ -1,3 +1,48 @@
 // -D (apni application pr jo ha load ni daln ga ma apny liye ya dependencies krrha hn)
 
 // gitignore jo hy wo nodemodules ki files ko ni push kry ga git ignore kun ky ya jo ha dev ky liye ha
+
+//javascript var globally or reassign bh hoskta ha
+
+// let reassginable const not-reassignable
+
+// case sensitive example camelCase , snake_case, PascalCase
+
+// tsc -w sy bar bar compile ni krna pry ga
+
+// let ma value change ni krskty ha eg let val = "ff" let val = "gg" error ay ga agr krni ha change to val = "dd"
+// var ma value change  krskty ha eg var val = "ff" var val = "gg" error ni ay ga val change hokr gg hojay ga
+// var ma ya problem thi production level ky waqt ya masla ata tha ky bd ma ya redefine hojata ha
+// let ma ya ni hota
+
+//const jo ha for example company name jo kbhi bh change ni hota
+
+//  Variables
+let fav = "ff"
+
+let favColor //camel case
+let fav_Color //snake_case
+let FavColor //PascalCase
+console.log(fav);
+
+let myName = "furqan" //camel case
+
+myName = "talha" //camel case
+let my_name = "furqan" //snake case
+let MyName = "furqan" //pascal case
+
+
+console.log(myName);
+console.log(my_name);
+console.log(MyName);
+
+// Data Types
+
+let dataType1: string = "Muhammad Talha"
+let dataType2: number = 24
+let dataType3: boolean = true
+console.log(dataType1);
+console.log(dataType2);
+console.log(dataType3);
+
+
